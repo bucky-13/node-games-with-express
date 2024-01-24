688ca1d95b2f1d00da5eb5d75f461636b4ef9284
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-01-24 10:58:55 +0100

    added games array for json file, updated get/patch etc

--- routes/games.js
+++ routes/games.js
@@ -2,76 +2,143 @@ var express = require('express');
 var router = express.Router();
 let cors = require('cors');
 
+const fs = require('fs');
+const { Z_FILTERED } = require('zlib');
+
 router.use(cors());
 
-const games = [
-  {
-    id: '1',
-    gameName: "Baldur's Gate 3",
-    developer: 'Larian',
-    published: 2023,
-    installed: false,
-  },
-  {
-    id: '2',
-    gameName: 'Civilization VI',
-    developer: 'Firaxis Games',
-    published: 2016,
-    installed: false,
-  },
-  {
-    id: '3',
-    gameName: 'Valheim',
-    developer: 'Iron Gate',
-    published: 2021,
-    installed: true,
-  },
-  {
-    id: '4',
-    gameName: 'Elder Scrolls Online',
-    developer: 'ZOS Interactive',
-    published: 2012,
-    installed: false,
-  },
-];
-
 router.get('/', (req, res) => {
-  res.json(games);
+  fs.readFile('games.json', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      const games = JSON.parse(data);
+      res.send(games);
+    }
+  });
 });
 
 router.get('/:gameId', (req, res) => {
-  let index = req.params.gameId;
-  let game = games.find((game) => game.id == index);
-  res.send(game);
+  fs.readFile('games.json', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      const games = JSON.parse(data);
+      let index = req.params.gameId;
+      let game = games.find((game) => game.id == index);
+      res.send(game);
+    }
+  });
 });
 
 router.post('/savegame', function (req, res) {
-  let yearPublished = Number(req.body.published);
+  fs.readFile('games.json', function (err, data) {
+    if (err) {
+      console.log(err);
 
-  let newGame = {
-    id: games.length + 1,
-    gameName: req.body.gameName,
-    developer: req.body.developer,
-    published: yearPublished,
-    installed: false,
-  };
+      if (err.code == 'ENOENT') {
+        console.log('file does not exist');
 
-  games.push(newGame);
-  res.send(newGame);
+        let games = {
+          id: `1`,
+          gameName: req.body.gameName,
+          developer: req.body.developer,
+          published: yearPublished,
+          installed: false,
+        };
+
+        fs.writeFile(
+          'games.json',
+          JSON.stringify(games, null, 2),
+          function (err) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+        res.send(games);
+        return;
+      }
+      res.send('404, things went wrong');
+    } else {
+      const games = JSON.parse(data);
+
+      let yearPublished = Number(req.body.published);
+
+      let newGame = {
+        id: `${games.length + 1}`,
+        gameName: req.body.gameName,
+        developer: req.body.developer,
+        published: yearPublished,
+        installed: false,
+      };
+
+      games.push(newGame);
+
+      fs.writeFile(
+        'games.json',
+        JSON.stringify(games, null, 2),
+        function (err) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+      res.send(newGame);
+    }
+  });
 });
 
 router.patch('/:gameId/install', function (req, res) {
-  let index = req.params.gameId;
-  let game = games.find((game) => game.id == index);
-  game.installed = true;
-  res.send(game);
+  fs.readFile('games.json', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const games = JSON.parse(data);
+      let index = req.params.gameId;
+      let game = games.find((game) => game.id == index);
+      game.installed = true;
+
+      fs.writeFile(
+        'games.json',
+        JSON.stringify(games, null, 2),
+        function (err) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+
+      res.send(game);
+      return;
+    }
+  });
 });
 
 router.patch('/:gameId/uninstall', function (req, res) {
-  let index = req.params.gameId;
-  let game = games.find((game) => game.id == index);
-  game.installed = false;
-  res.send(game);
+  fs.readFile('games.json', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const games = JSON.parse(data);
+      let index = req.params.gameId;
+      let game = games.find((game) => game.id == index);
+      game.installed = false;
+
+      fs.writeFile(
+        'games.json',
+        JSON.stringify(games, null, 2),
+        function (err) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+
+      res.send(game);
+      return;
+    }
+  });
 });
 
 module.exports = router;
