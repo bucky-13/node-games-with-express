825095f3e741ce1a97e3ff516e48bc56e4b18705
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-01-29 12:41:48 +0100

    fixed image upload functionality

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ var indexRouter = require('./routes/index');
 var gamesRouter = require('./routes/games');
 
 var app = express();
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 })
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static('/stylesheets/style.css'));
