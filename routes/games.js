83ca95dadcfdab8416f595edcd0daba0bbdc8ae1
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-01-29 10:06:38 +0100

    added images to the page

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 let cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg',
+  'image/webp': 'webp',
+};
+
+// const upload = multer({ dest: 'public/images' });
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images');
+  },
+  filename: function (req, file, cb) {
+    fs.readFile('games.json', function (err, data) {
+      if (err) {
+        console.log('err ', err);
+      } else {
+        const games = JSON.parse(data);
+        let index = req.params.gameId;
+        let game = games.find((game) => game.id == index);
+        let imgName = game.gameName
+          .replace(/[^a-z0-9\s]/gi, '')
+          .replace(/[_\s]/g, '-');
+
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, imgName + '.' + ext);
+      }
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const fs = require('fs');
 
@@ -9,7 +45,7 @@ router.use(cors());
 router.get('/', (req, res) => {
   fs.readFile('games.json', function (err, data) {
     if (err) {
-      console.log(err);
+      console.log('err ', err);
     } else {
       const games = JSON.parse(data);
       res.send(games);
@@ -17,10 +53,23 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/:gameId/upload', (req, res) => {
+  fs.readFile('games.json', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const games = JSON.parse(data);
+      let index = req.params.gameId;
+      let game = games.find((game) => game.id == index);
+      res.send(game);
+    }
+  });
+});
+
 router.get('/:gameId', (req, res) => {
   fs.readFile('games.json', function (err, data) {
     if (err) {
-      console.log(err);
+      console.log('err ', err);
     } else {
       const games = JSON.parse(data);
       let index = req.params.gameId;
@@ -33,7 +82,7 @@ router.get('/:gameId', (req, res) => {
 router.post('/savegame', function (req, res) {
   fs.readFile('games.json', function (err, data) {
     if (err) {
-      console.log(err);
+      console.log('err ', err);
 
       if (err.code == 'ENOENT') {
         console.log('file does not exist');
@@ -51,7 +100,7 @@ router.post('/savegame', function (req, res) {
           JSON.stringify(games, null, 2),
           function (err) {
             if (err) {
-              console.log(err);
+              console.log('err ', err);
             }
           }
         );
@@ -65,11 +114,12 @@ router.post('/savegame', function (req, res) {
       let yearPublished = Number(req.body.published);
 
       let newGame = {
-        id: `${games.length + 1}`,
+        id: uuidv4(),
         gameName: req.body.gameName,
         developer: req.body.developer,
         published: yearPublished,
         installed: false,
+        image: '',
       };
 
       games.push(newGame);
@@ -79,7 +129,7 @@ router.post('/savegame', function (req, res) {
         JSON.stringify(games, null, 2),
         function (err) {
           if (err) {
-            console.log(err);
+            console.log('err ', err);
           }
         }
       );
@@ -91,49 +141,22 @@ router.post('/savegame', function (req, res) {
 router.patch('/:gameId/install', function (req, res) {
   fs.readFile('games.json', (err, data) => {
     if (err) {
-      console.log(err);
+      console.log('err ', err);
     } else {
       const games = JSON.parse(data);
       let index = req.params.gameId;
       let game = games.find((game) => game.id == index);
-      game.installed = true;
+      game.installed = game.installed ? false : true;
 
       fs.writeFile(
         'games.json',
         JSON.stringify(games, null, 2),
         function (err) {
           if (err) {
-            console.log(err);
+            console.log('err ', err);
           }
         }
       );
-
-      res.send(game);
-      return;
-    }
-  });
-});
-
-router.patch('/:gameId/uninstall', function (req, res) {
-  fs.readFile('games.json', (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const games = JSON.parse(data);
-      let index = req.params.gameId;
-      let game = games.find((game) => game.id == index);
-      game.installed = false;
-
-      fs.writeFile(
-        'games.json',
-        JSON.stringify(games, null, 2),
-        function (err) {
-          if (err) {
-            console.log(err);
-          }
-        }
-      );
-
       res.send(game);
       return;
     }
@@ -143,7 +166,7 @@ router.patch('/:gameId/uninstall', function (req, res) {
 router.delete('/:gameId', (req, res) => {
   fs.readFile('games.json', (err, data) => {
     if (err) {
-      console.log(err);
+      console.log('err ', err);
     } else {
       let games = JSON.parse(data);
       let index = req.params.gameId;
@@ -155,11 +178,35 @@ router.delete('/:gameId', (req, res) => {
         JSON.stringify(games, null, 2),
         function (err) {
           if (err) {
-            console.log(err);
+            console.log('err ', err);
           }
         }
       );
       res.send(deletedGame);
+    }
+  });
+});
+
+router.post('/:gameId/saveimage', upload.single('image'), (req, res) => {
+  fs.readFile('games.json', (err, data) => {
+    if (err) {
+      console.log('err', err);
+    } else {
+      let games = JSON.parse(data);
+      let index = req.params.gameId;
+      let game = games.find((game) => game.id == index);
+      game.image = `http://localhost:3000/images/${req.file.filename}`;
+
+      fs.writeFile(
+        'games.json',
+        JSON.stringify(games, null, 2),
+        function (err) {
+          if (err) {
+            console.log('err', err);
+          }
+        }
+      );
+      res.send(game);
     }
   });
 });
